252a5fe2035f66a9be3e4466e3df3ac34e144588
guemarwa2021-git <93319714+guemarwa2021-git@users.noreply.github.com>
2022-01-30 21:41:28 +0100

    Add files via upload

---
+++ ceasar.c
@@ -0,0 +1,29 @@
+#include "ceasar.h"
+#include <string.h>
+#include <ctype.h>
+
+void caesarEnc(const char * message, short key, char * ciphertext){
+    unsigned int i = 0;
+    short rang;
+    while( message[i] != '\0'){
+        if( ( message[i] >= 'a') && (message[i] <= 'z') ){
+            rang = (message[i] - 'a' + key) % 26;
+            if( rang < 0) rang += 26;
+            ciphertext[i] = 'a' + rang;
+        }
+        else if(( message[i] >= 'A') && (message[i] <= 'Z')){
+            rang = (message[i] - 'A' + key) % 26;
+            if( rang < 0) rang += 26;
+            ciphertext[i] = 'A' + rang;
+        }
+        else {
+            ciphertext[i] = message[i];
+        }
+        i++;
+    }
+    ciphertext[i] = '\0';
+}
+
+void caesarDec(const char * ciphertext, short key, char * plaintext){
+    caesarEnc(ciphertext, -key, plaintext);
+}
